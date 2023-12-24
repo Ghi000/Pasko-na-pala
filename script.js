8adbda5f1ef0e40dac817e2d5f5dc4e26f73541a
Ghi000 <153405810+Ghi000@users.noreply.github.com>
2023-12-24 20:53:01 +0800

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector(".wrapper");
 const gif = document.querySelector(".gif");
   question.innerHTML = "Merry Christmas Mahal";
   gif.src =
-    "merrychristmas_n";
+    "merrychristmas_n.mp4";
 });
 
 
