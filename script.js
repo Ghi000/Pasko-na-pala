6efad079dc2f6569be410aadf349e7df29e494d9
Ghi000 <153405810+Ghi000@users.noreply.github.com>
2023-12-24 21:03:34 +0800

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector(".wrapper");
 const gif = document.querySelector(".gif");
-  question.innerHTML = "Merry Christmas Mahal";
+  question.innerHTML = "Pasko-na-pala
+/merrychristmas_n.mp4
+";
   gif.src =
     "merrychristmas_n.mp4";
 });
